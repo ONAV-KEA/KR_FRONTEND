18e9192427d2d3c790c27aa5685dbb6082d17796
Teller501 <ande777j@stud.kea.dk>
2023-11-23 15:21:19 +0100

    added pagination

--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,24 @@
-const api = "http://localhost:8080/api/event"
+const API = "http://localhost:8080/api/event"
+const PAGE_SIZE = 10;
+let sortColumn = "name";
+let sortDirection = 'asc';
+let name = "";
+let startDate = "";
+let endDate = "";
+let loc = "";
 
 document.getElementById("openbtn").addEventListener("click", openNav);
 document.getElementById("closebtn").addEventListener("click", closeNav);
 document.getElementById("add-event-btn").addEventListener("click", showAddEventModal);
+document.getElementById('header-row').onclick = function (evt) {
+    const id = evt.target.id;
+    if(!id.startsWith('sort-')) return;
+    //TODO handle sorting here
+      sortColumn = id.substring(5);
+      sortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+    makeEventRows();
+  };
+
 
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
@@ -47,7 +63,7 @@ function addEvent() {
         },
         body: JSON.stringify(eventData)
     }
-    fetch(`${api}`, options)
+    fetch(`${API}`, options)
     .then(response => response.json())
     .then(data => {
         console.log(data);
@@ -56,39 +72,83 @@ function addEvent() {
     myModal.hide();
 }
 
-async function makeEventRows(){
-    const events = await getAllEvents();
-        const tableBody = document.getElementById("events-table-body");
+function makeEventRows(events) {
+    const tableBody = document.getElementById("events-table-body");
+ 
+    const rows = events.map(event => {
+        return `
+        <tr>
+            <td>${event.name}</td>
+            <td>${event.startDate}</td>
+            <td>${event.endDate}</td>
+            <td>${event.location}</td>
+            <td>
+                <button class="btn btn-primary btn-sm">View Event</button>
+            </td>
+            <td>
+                <button class="btn btn-warning btn-sm">Edit</button>
+            </td>
+            <td>
+                <button class="btn btn-danger btn-sm">Delete</button>
+            </td>
+        </tr>
+        `;
+    });
+ 
+    tableBody.innerHTML = rows.join("");
+ }
+ 
 
-        const rows = events.content.map(event => {
-            return `
-            <tr>
-                <td>${event.name}</td>
-                <td>${event.startDate}</td>
-                <td>${event.endDate}</td>
-                <td>${event.location}</td>
-                <td>
-                    <button class="btn btn-primary btn-sm" onclick="showEditEventModal(${event.id})">Edit</button>                </td>
-            </tr>
-            `;
-        });
 
-        tableBody.innerHTML = rows.join("");
-}
-// TODO: add paging
-function getAllEvents() {
-    return fetch(api)
+ function getAllEvents(page = 0, size = PAGE_SIZE, sort = sortColumn) {
+    return fetch(`${API}?page=${page}&size=${size}&sort=${sort},${sortDirection}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            makeEventRows(data.content);
+            displayPagination(data.totalPages, page);
             return data;
         })
         .catch(error => {
             console.error('Error fetching events:', error);
             throw error; // Rethrow the error to propagate it to the caller
         });
+ }
+ 
+
+
+function displayPagination(totalPages, currentPage) {
+    let paginationHtml = '';
+    const paginationElement = document.getElementById('pagination');
+    
+    if (currentPage > 0) {
+        paginationHtml += `<li class="page-item"><a class="page-link" data-page="${currentPage - 1}" href="#">Previous</a></li>`;
+    }
+
+    let startPage = Math.max(0, currentPage - 2);
+    let endPage = Math.min(totalPages - 1, currentPage + 2);
+
+    for (let i = startPage; i <= endPage; i++) {
+        if (i === currentPage) {
+            paginationHtml += `<li class="page-item active"><a class="page-link" href="#">${i + 1}</a></li>`;
+        } else {
+            paginationHtml += `<li class="page-item"><a class="page-link" data-page="${i}" href="#">${i + 1}</a></li>`;
+        }
+    }    
+
+    if (currentPage < totalPages - 1) {
+        paginationHtml += `<li class="page-item"><a class="page-link" data-page="${currentPage + 1}" href="#">Next</a></li>`;
+    }
+
+    paginationElement.innerHTML = paginationHtml;
 }
 
+document.querySelector('#pagination').onclick = function (evt) {
+    evt.preventDefault();
+    if (evt.target.tagName === 'A' && evt.target.hasAttribute('data-page')) {
+        const page = parseInt(evt.target.getAttribute('data-page'));
+        getAllEvents(page);
+    }
+};
 
-
-makeEventRows();
+getAllEvents(0);
