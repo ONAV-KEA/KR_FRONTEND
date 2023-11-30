bf0db28c08a8f6bd21116cf93fd972154e6562fc
Teller501 <ande777j@stud.kea.dk>
2023-11-30 13:42:36 +0100

    added login with jwt

---
+++ js/security.js
@@ -0,0 +1,59 @@
+const API = 'http://localhost:8080/api/user';
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('loginForm').addEventListener('submit', login);
+});
+
+function login(event) {
+    event.preventDefault();
+    const usernameField = document.getElementById("usernameField").value;
+    const passwordField = document.getElementById("passwordField").value;
+    let payload = {
+        username: usernameField,
+        password: passwordField
+    };
+    payload = JSON.stringify(payload);
+    fetch(`${API}/login`, {
+        method: "POST",
+        body: payload,
+        headers: {'content-type': 'application/json'}
+    })
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error('Wrong username or password');
+            }
+        })
+        .then(async function (data) {
+            console.log(data);
+            localStorage.setItem('user', JSON.stringify(data));
+            const user = await getUserByToken()
+            if (user.role === 'MANAGER' || user.role === 'HEADCHEF'){
+                window.location.href = "admin.html";
+            } else{
+                window.location.href = "events.html";
+            }
+        })
+        .catch(function (error) {
+            console.log(error.message, "red");
+        });
+}
+
+
+function getToken(){
+    const localstorage_user = JSON.parse(localStorage.getItem('user'))
+    return  localstorage_user.token
+}
+
+function getUserByToken(){
+    const localstorage_user = JSON.parse(localStorage.getItem('user'))
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localstorage_user.token}`
+        }
+    }
+    return fetch(`${API}?token=${localstorage_user.token}`, options)
+    .then(res => res.json())
+}
